4790cce23c63e97980f8097bf3483f0134ce92d7
Максим Максим <maximzombi@yandex.ru>
2022-05-29 03:24:53 +0400

    Add project files.

---
+++ GraphCalculationHelper/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace GraphCalculationHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers count:");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            var fileName = $"Result_{n}.txt";
+
+            var input = File.ReadAllLines(fileName);
+            var graphsCount = 0L;
+            var savingGroupSizeCount = 0L;
+
+            foreach (var str in input)
+            {
+                if (string.IsNullOrEmpty(str))
+                {
+                    continue;
+                }
+
+                graphsCount += long.Parse(new string(str.Split('=')[2].Skip(1).TakeWhile(x => x != ';').ToArray()));
+                savingGroupSizeCount += long.Parse(new string(str.Split('=')[3].Skip(1).TakeWhile(x => x != ';').ToArray()));
+            }
+
+            var averageSavingGroupSizeCount = (savingGroupSizeCount + .0) / (graphsCount - 1);
+
+            Console.WriteLine($"Total graphs saving group size: {savingGroupSizeCount}");
+            Console.WriteLine($"Average graphs saving group size: {averageSavingGroupSizeCount}");
+        }
+    }
+}
